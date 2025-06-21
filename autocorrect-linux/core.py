773734df450a74518184d23a0e70fcd4d9bb84c0
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-21 13:55:57 +0200

    transition to pynput + add simple keylogger

--- autocorrect-linux/core.py
+++ autocorrect-linux/core.py
@@ -1,4 +1,26 @@
 import click
+from pynput.keyboard import Key, Listener
 
+global letters
+letters = []
+
+def on_press(key):
+    if hasattr(key, "char") and key.char is not None:
+        letters.append(key.char)
+    elif key == Key.backspace:
+        try:
+            letters.pop()
+        except IndexError:
+            pass
+    elif key == Key.space or Key.enter:
+        letters.append(" ")
+    elif key == Key.esc:
+        return False
+    
+    click.echo("".join(letters))
+    
 def main():
-    click.secho("Hello World!", fg="green")
+    with Listener(
+        on_press=on_press
+    ) as listener:
+        listener.join()
