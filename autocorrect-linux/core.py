57658053e4b9e5bb4c1700170e6d5a390c6938a3
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-22 12:16:26 +0200

    forgot to commit (changed a lot)

--- autocorrect-linux/core.py
+++ autocorrect-linux/core.py
@@ -1,20 +1,22 @@
 import os
 import click
 import enchant
-from subprocess import PIPE, Popen
 import enchant.pypwl
+from . import utils
 from pynput.keyboard import Key, Listener
 
+# set variables
 gui_root = None
 text1 = None
 text2 = None
 text3 = None
 
+# dictionaries to store letters, texts, and learned words
 letters = {}
 texts = {}
 learned_words = {}
 
-pwl_path = "pwl.txt"
+pwl_path = os.path.join(utils.data_dir, "pwl.txt")
 
 if not os.path.exists(pwl_path):
     open(pwl_path, 'a').close()
@@ -23,23 +25,17 @@ if not os.path.exists(pwl_path):
 pwl = enchant.pypwl.PyPWL(pwl_path)
 dictionary = enchant.DictWithPWL("en", pwl_path)
 
-# get id of focused window
-def get_window_id():
-    try:
-        root = Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=PIPE, stderr=PIPE)
-        root_output, _ = root.communicate()
-        
-        if root_output:
-            for line in root_output.decode().split('\n'):
-                if "_NET_ACTIVE_WINDOW(WINDOW):" in line:
-                    parts = line.split()
-                    for part in parts:
-                        if part.startswith('0x') and len(part) > 3:
-                            window_id = part.rstrip(',')  # Remove trailing comma
-                            return window_id
-    except Exception as e:
-        click.echo(e)
-    return "Unknown"
+def get_suggestions(word):
+    valid_suggestions = []
+    
+    suggestions = dictionary.suggest(word)
+    if suggestions:
+        for suggestion in suggestions:
+            if suggestion.startswith(word):
+                valid_suggestions.append(suggestion)
+        return valid_suggestions
+    else:
+        return False
 
 def update_suggestions(suggestions):
     if gui_root and text1:
@@ -54,16 +50,20 @@ def update_suggestions(suggestions):
                 text3.delete("1.0", "end")
                 text3.insert("1.0", suggestions[2])
         gui_root.after(0, update_text)
-        
+
+def suggest_next():
+    update_suggestions([None, "Next Word 1", "Next Word 2"])
+
 def suggest(last_word, text, words):
     update_suggestions([f'"{last_word}"', None, None])
     if last_word:
-        suggestions = dictionary.suggest(last_word)
-        if suggestions:
+        suggestions = get_suggestions(last_word)
+        if suggestions and len(suggestions) >= 2:
             update_suggestions([None, suggestions[0], suggestions[1]])
-            #print(suggestions)
+        elif suggestions and len(suggestions) == 1:
+            update_suggestions([None, suggestions[0], ""])
         else:
-            click.secho(f"No suggestions for: {last_word}", fg="yellow")
+            update_suggestions([None, "", ""])
     elif text.endswith(" "):
         if words[-2]:
             if not dictionary.check(words[-2]):
@@ -77,15 +77,15 @@ def suggest(last_word, text, words):
                     pwl.add_to_pwl(last_word)
                     click.secho(f"Added {last_word} to PWL", fg="blue")
             
-            click.echo("Suggest next word")
+            suggest_next()
         else:
-            click.echo("Suggest next word")
+            suggest_next()
 
 def on_press(key):
     global gui_root, text1, text2, text3
     global letters, texts, learned_words, pwl
 
-    current_window_id = str(get_window_id())
+    current_window_id = str(utils.get_window_id())
     if current_window_id not in letters:
         letters[current_window_id] = []
     if hasattr(key, "char") and key.char is not None:
@@ -111,7 +111,9 @@ def on_press(key):
                     suggest(last_word, text, words)
             else:
                 suggest(last_word, text, words)
-    
+                    
 def main():
-    with Listener(on_press=on_press) as listener:
+    with Listener(
+        on_press=on_press
+    ) as listener:
         listener.join()
---
+++ autocorrect-linux/utils.py
@@ -0,0 +1,45 @@
+import os
+import click
+import requests
+from subprocess import PIPE, Popen
+
+data_dir = os.path.expanduser("~/.local/share/autocorrect-linux")
+os.makedirs(data_dir, exist_ok=True)
+
+# get id of focused window
+def get_window_id():
+    try:
+        root = Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=PIPE, stderr=PIPE)
+        root_output, _ = root.communicate()
+        
+        if root_output:
+            for line in root_output.decode().split('\n'):
+                if "_NET_ACTIVE_WINDOW(WINDOW):" in line:
+                    parts = line.split()
+                    for part in parts:
+                        if part.startswith('0x') and len(part) > 3:
+                            window_id = part.rstrip(',')  # Remove trailing comma
+                            return window_id
+    except Exception as e:
+        click.echo(e)
+    return "Unknown"
+
+def load_dictionary():
+    dict_path = os.path.join(data_dir, "words_alpha.txt")
+    try:
+        if not os.path.exists(dict_path):
+            click.echo("Downloading dictionary...")
+            response = requests.get("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt")
+            if response.status_code == 200:
+                with open(dict_path, 'w', encoding='utf-8') as f:
+                    f.write(response.text)
+            else:
+                click.echo(f"Failed to download dictionary: {response.status_code}")
+                return []
+        click.echo("Loading dictionary...")
+        with open(dict_path, 'r', encoding='utf-8') as f:
+            words = sorted(line.strip() for line in f)
+        return words
+    except requests.RequestException as e:
+        click.echo(f"Error loading dictionary: {e}")
+        return []
