4b8fed1fbcdf59290a83d12c51fe60d2c7a6117d
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-23 19:47:07 +0200

    renamed folder

---
+++ autocorrect_linux/__init__.py
@@ -0,0 +1,6 @@
+"""
+System-wide autocorrect for Linux.
+"""
+
+__version__ = "0.1.0"
+__author__ = "simon0302010"
---
+++ autocorrect_linux/core/gui.py
@@ -0,0 +1,46 @@
+import customtkinter
+from pynput.mouse import Controller
+
+def build_gui():
+    mouse = Controller()
+    root = customtkinter.CTk()
+
+    width = 350
+    height = 80
+
+    x, y = mouse.position
+
+    customtkinter.set_appearance_mode("System")
+
+    root.geometry(f"{width}x{height}+{x - width // 2}+{y - height // 2}")
+    root.overrideredirect(True)
+
+    for i in range(4):
+        root.columnconfigure(i, weight=1 if i < 3 else 0)
+    root.rowconfigure(0, minsize=30)
+    root.rowconfigure(1, weight=1)
+    
+    stats = customtkinter.CTkTextbox(root, border_width=0, height=30, text_color="gray")
+    stats.grid(row=0, column=0, columnspan=3, sticky='nsew')
+
+    text1 = customtkinter.CTkTextbox(root, border_width=2)
+    text1.grid(row=1, column=0, sticky='nsew')
+
+    text2 = customtkinter.CTkTextbox(root, border_width=2)
+    text2.grid(row=1, column=1, sticky='nsew')
+
+    text3 = customtkinter.CTkTextbox(root, border_width=2)
+    text3.grid(row=1, column=2, sticky='nsew')
+
+    close_button = customtkinter.CTkButton(
+        root,
+        text="X",
+        command=root.destroy,
+        fg_color="#ff1100",
+        hover_color="#cf4d44",
+        width=30,
+        height=20
+    )
+    close_button.place(x=width-35, y=5)
+
+    return root, (text1, text2, text3, stats)
